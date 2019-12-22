c516b94946de93a6fa78e588b745a73225e68642
sebastian_bienert <226086@student.pwr.edu.pl>
2019-12-22 20:13:30 +0100

    Added dummy test project for jenkins pipeline

---
+++ PWRSyllabus.Tests/DummyTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PWRSyllabus.Tests
+{
+    public class DummyTests
+    {
+        [Fact]
+        public void DummyTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
